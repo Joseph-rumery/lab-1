3a50ea24c9fa172be6cc93fb7b653756649a5895
Joseph-rumery <144263711+Joseph-rumery@users.noreply.github.com>
2023-09-13 14:32:59 -0400

    Add files via upload

---
+++ Fraction.java
@@ -0,0 +1,20 @@
+public class Fraction implements INumber<Fraction> {
+  //numerator
+  //denominator
+	
+	 public Fraction plus(Fraction input) {
+		 return null;
+	 }
+	 public Fraction minus(Fraction input) {
+		  return null;
+	  }
+	 public Fraction divide(Fraction input) {
+		  return null;
+	  }
+	 public Fraction multiply(Fraction input) {
+		  return null;
+	  }
+	 public void print() {
+		  
+	  }
+}
